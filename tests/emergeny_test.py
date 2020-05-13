47e8af1055c38c399c8ee88c43aa3630239fbc25
Onayemi H. Ademola <onayemi.hakeem@gmail.com>
2020-05-13 21:26:30 +0100

    emergency _test

---
+++ tests/emergeny_test.py
@@ -0,0 +1,9 @@
+from django.test import TestCase
+from good-health-server.models import EmergencyLine
+
+class EmergencyLineTestCsase(TestCase):
+    def setUp(self):
+        name = models.CharField(max_length=100)
+        phone_number = models.CharField(max_length=15)
+        assertTrue()
+        
